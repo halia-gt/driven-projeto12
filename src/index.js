f79a7cb0cd6f7e5c5e2e8aa3c97439f67ebcf762
Nathalia <nathalia.gramst@gmail.com>
2022-09-01 15:59:38 -0300

    Get participants and checking already existing usernames

--- src/index.js
+++ src/index.js
@@ -19,26 +19,42 @@ let db;
     try {
         await client.connect();
         db = client.db('batepapo-uol');
-        console.log('ok')
     } catch (error) {
         console.log(error);
     }
 })();
 
+const getParticipants = async () => {
+    const participants = await db.collection('participants').find().toArray();
+    return participants;
+}
+
+const duplicatedUsername = async (name) => {
+    const participants = await getParticipants();
+    return participants.some(participant => (participant.name === name))
+}
+
 app.post('/participants', async (req, res) => {
     try {
         const { name } = req.body;
 
-        if(!name) {
-            res.status(422).send({ message: 'Todos os campos são obrigatórios' }); //AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH JOI
+        if (!name) {
+            res.status(422).send({ message: 'Todos os campos são obrigatórios' });
+            return; //AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH JOI
         }
 
-        // fazer verificação se o nome já está sendo usado
+        const participants = await getParticipants();
+
+        if (await duplicatedUsername(name)) {
+            res.status(409).send({ message: 'Usuário inválido' });
+            return;
+        }
 
         await db.collection('participants').insertOne({
             name,
             lastStatus: Date.now()
         });
+
         await db.collection('messages').insertOne({
             from: name,
             to: 'Todos',
@@ -53,8 +69,20 @@ app.post('/participants', async (req, res) => {
         console.error(error);
         res.sendStatus(500);
     }
-})
+});
+
+app.get('/participants', async (req, res) => {
+    try {
+        const participants = await getParticipants();
+        res.send(participants);
+        
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+    
+});
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');
-})
+});
